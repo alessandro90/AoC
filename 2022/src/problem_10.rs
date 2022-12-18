f5b327a0897cb656020b68da633e9521bc2fdb09
Alessandro <alessandro.ceccato.3@gmail.com>
2022-12-18 20:08:42 +0100

    problem 10 part 2

--- 2022/src/problem_10.rs
+++ 2022/src/problem_10.rs
@@ -3,8 +3,8 @@
 use crate::utilities::read_file;
 
 fn get_input() -> String {
-    read_file("problem_10_sample")
-    // read_file("problem_10_input")
+    // read_file("problem_10_sample")
+    read_file("problem_10_input")
 }
 
 fn parse_input() -> Vec<Instruction> {
@@ -14,7 +14,7 @@ fn parse_input() -> Vec<Instruction> {
             if ln.starts_with("noop") {
                 Instruction::Noop
             } else {
-                let n: i64 = ln.split_once(' ').unwrap().1.parse().unwrap();
+                let n: i32 = ln.split_once(' ').unwrap().1.parse().unwrap();
                 Instruction::Addx(n)
             }
         })
@@ -23,12 +23,65 @@ fn parse_input() -> Vec<Instruction> {
 
 enum Instruction {
     Noop,
-    Addx(i64),
+    Addx(i32),
+}
+
+const SCREEN_HEIGHT: usize = 6;
+const SCREEN_WIDTH: usize = 40;
+
+type Screen = [[char; SCREEN_WIDTH]; SCREEN_HEIGHT];
+
+struct Crt {
+    pixels: Screen,
+    sprite_center: i32,
+    row: usize,
+    col: usize,
+}
+
+impl Crt {
+    fn new() -> Self {
+        Self {
+            pixels: [[' '; SCREEN_WIDTH]; SCREEN_HEIGHT],
+            sprite_center: 1,
+            row: 0,
+            col: 0,
+        }
+    }
+
+    fn set_sprite_center(&mut self, center: i32) {
+        self.sprite_center = center;
+    }
+
+    fn draw_pixel(&mut self) {
+        if self.is_pixel_visible() {
+            self.pixels[self.row][self.col] = '#';
+        }
+        if self.col == SCREEN_WIDTH - 1 {
+            self.col = 0;
+            self.row += 1;
+        } else {
+            self.col += 1;
+        }
+    }
+
+    fn is_pixel_visible(&self) -> bool {
+        let col = self.col as i32;
+        (col >= self.sprite_center && col - self.sprite_center <= 1)
+            || (col < self.sprite_center && self.sprite_center - col == 1)
+    }
+
+    fn display(&self) -> String {
+        self.pixels.into_iter().fold("".to_owned(), |acc, row| {
+            let r = row.iter().collect::<String>();
+            acc + &r + "\n"
+        })
+    }
 }
 
 struct Cpu {
-    reg: i64,
-    cycles: Vec<i64>,
+    reg: i32,
+    cycles: Vec<i32>,
+    crt: Crt,
 }
 
 impl Cpu {
@@ -36,12 +89,14 @@ impl Cpu {
         Self {
             reg: 1,
             cycles: vec![],
+            crt: Crt::new(),
         }
     }
 
     fn cycle(&mut self, times: u32) {
         for _ in 0..times {
             self.cycles.push(self.reg);
+            self.crt.draw_pixel();
         }
     }
 
@@ -51,16 +106,17 @@ impl Cpu {
             Instruction::Addx(x) => {
                 self.cycle(2);
                 self.reg += x;
+                self.crt.set_sprite_center(self.reg);
             }
         };
     }
 
-    fn signal_strength(&mut self, cycle_nr: usize) -> i64 {
-        self.cycles[cycle_nr - 1] * (cycle_nr as i64)
+    fn signal_strength(&mut self, cycle_nr: usize) -> i32 {
+        self.cycles[cycle_nr - 1] * (cycle_nr as i32)
     }
 }
 
-fn solution_part_1() -> i64 {
+fn solution_part_1() -> i32 {
     let instructions = parse_input();
     let mut cpu = Cpu::new();
     for instruction in instructions {
@@ -72,8 +128,13 @@ fn solution_part_1() -> i64 {
         .sum()
 }
 
-fn solution_part_2() -> u64 {
-    todo!()
+fn solution_part_2() -> String {
+    let instructions = parse_input();
+    let mut cpu = Cpu::new();
+    for instruction in instructions {
+        cpu.execute(instruction);
+    }
+    cpu.crt.display()
 }
 
 #[cfg(test)]
@@ -87,6 +148,6 @@ mod tests {
 
     #[test]
     fn problem_10_solution_part_2_test() {
-        println!("problem 10 solution 2: {}", solution_part_2());
+        println!("problem 10 solution 2: \n{}", solution_part_2());
     }
 }
