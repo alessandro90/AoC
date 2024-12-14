d7db92d3c2010a000d6f897a276b5522fa25a71a
alessandro90 <alessandro.ceccato.3@gmail.com>
2024-12-14 19:34:06 +0100

    2024 - day 3 - part 2

--- 2024/aoc/src/Day3.hs
+++ 2024/aoc/src/Day3.hs
@@ -1,15 +1,23 @@
-module Day3 where
+module Day3 (part1, part2) where
 
-import Control.Applicative ((<|>))
 import Control.Monad (void)
-import Text.Parsec (ParseError, anyChar, char, digit, many, many1, parse, string, try)
-import Text.Parsec.String (Parser)
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char (char, digitChar, latin1Char, string)
 
-part1 :: FilePath -> IO (Either ParseError Int)
+type Parser = Parsec Void String
+
+part1 :: FilePath -> IO (Either (ParseErrorBundle String Void) Int)
 part1 path = part1' <$> readFile path
 
-part1' :: String -> Either ParseError Int
-part1' s = computeSumOfMul <$> parse' inputParser s
+part1' :: String -> Either (ParseErrorBundle String Void) Int
+part1' s = computeSumOfMul <$> parse' parserPart1 s
+
+part2 :: FilePath -> IO (Either (ParseErrorBundle String Void) Int)
+part2 path = part2' <$> readFile path
+
+part2' :: String -> Either (ParseErrorBundle String Void) Int
+part2' s = computeSumOfMul <$> parse' parserPart2 s
 
 mul :: Parser (Int, Int)
 mul = string "mul" >> factors
@@ -24,15 +32,31 @@ factors = do
   return (x, y)
 
 integer :: Parser Int
-integer = read <$> many1 digit
+integer = read <$> some digitChar
 
-inputParser :: Parser [(Int, Int)]
-inputParser =
-  let p = try mul <|> try (anyChar >> p)
+parserPart1 :: Parser [(Int, Int)]
+parserPart1 =
+  let p = try mul <|> try (latin1Char >> p)
    in many p
 
 computeSumOfMul :: [(Int, Int)] -> Int
 computeSumOfMul = sum . map (uncurry (*))
 
-parse' :: Parser a -> String -> Either ParseError a
+parse' :: Parser a -> String -> Either (ParseErrorBundle String Void) a
 parse' p = parse p ""
+
+do_ :: Parser ()
+do_ = void $ string "do()"
+
+dont :: Parser ()
+dont = void $ string "don't()"
+
+comment :: Parser ()
+comment = dont >> void (manyTill latin1Char (do_ <|> eof))
+
+parserPart2 :: Parser [(Int, Int)]
+parserPart2 = many go
+  where
+    go = do
+      void $ optional $ try comment
+      try mul <|> try ((comment <|> void latin1Char) >> go)
